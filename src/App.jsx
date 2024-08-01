90601445d2a68b327a1fa01968b5cad286ffde49
Faiz Kirsten <faizkirsten@gmail.com>
2024-08-01 17:24:34 +0200

    Remove under construction page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import UnderConstruction from "./pages/UnderConstruction";
-
 // components
 import Header from "./components/Header";
 
 const App = () => {
-    const maintenance = false;
-    if (maintenance) return <UnderConstruction />;
-
     return (
         <div className="relative | ">
             <Header />
